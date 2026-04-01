d6e2ef2abc45224883fbdf89ccb2925840003650
Omid Kashan <omid@antilong.com>
2026-04-01 23:47:34 +0100

    feat(reddit): fix stupid search icon again

--- reddit.js
+++ reddit.js
@@ -3,17 +3,23 @@
 // @namespace   https://github.com/omidantilong/userscripts
 // @match       https://www.reddit.com/*
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      Omid Kashan
 // @description Various UI tweaks on reddit
 // ==/UserScript==
 
-const style = document.createElement('style')
+const style = document.createElement("style");
 
 style.textContent = `
-span:has(svg[icon-name="search-outline"]) {
+span.ms-2xs.align-middle:has(svg[icon-name="search"]) {
   display:none
 }
-`
+search-telemetry-tracker a {
+  text-decoration:none!important;
+  color:currentColor !important;
+  border-bottom:1px dashed hsl(from currentColor h s l / 0.3) !important;
 
-document.head.appendChild(style)
+}
+`;
+
+document.head.appendChild(style);
